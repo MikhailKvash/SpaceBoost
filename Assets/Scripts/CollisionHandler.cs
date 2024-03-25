af6310bc1d10c19d16d1790b3c39f056ea6e2378
MikhailKvash <mikhailkvash@gmail.com>
2024-03-25 19:33:07 +0300

    Scritps upd

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,22 +6,44 @@ public class CollisionHandler : MonoBehaviour
 {
   [SerializeField] private float _endDelay;
   
+  [SerializeField] private Movement _movement;
+  [SerializeField] private GameObject _visualParts;
   [SerializeField] private AudioClip _crashSound;
   [SerializeField] private AudioClip _finishSound;
+  [SerializeField] private ParticleSystem _crashParticle;
+  [SerializeField] private ParticleSystem _finishParticle;
 
-  private Movement _movement;
   private AudioSource _audioSource;
   private bool _isTransitioning;
+  private bool _collisionDisabled;
 
   private void Start()
   {
-    _movement = GetComponent<Movement>();
     _audioSource = GetComponent<AudioSource>();
   }
+  
+#if UNITY_EDITOR
+  private void Update()
+  {
+    RespondToDebugKeys();
+  }
+
+  private void RespondToDebugKeys()
+  {
+    if (Input.GetKeyDown(KeyCode.L))
+    {
+      LoadNextLevel();
+    }
+    else if (Input.GetKeyDown(KeyCode.C))
+    {
+      _collisionDisabled = !_collisionDisabled;
+    }
+  }
+#endif
 
   private void OnCollisionEnter(Collision other)
   {
-    if(_isTransitioning)
+    if(_isTransitioning || _collisionDisabled)
       return;
     
     switch (other.gameObject.tag)
@@ -40,16 +63,23 @@ public class CollisionHandler : MonoBehaviour
   private void StartSuccessSequence()
   {
     _isTransitioning = true;
+    _audioSource.Stop();
     _audioSource.PlayOneShot(_finishSound);
+    _finishParticle.Play();
     _movement.enabled = false;
+    _movement.StopParticles();
     Invoke(nameof(LoadNextLevel), _endDelay);
   }
 
   private void StartCrashSequence()
   {
     _isTransitioning = true;
+    _audioSource.Stop();
     _audioSource.PlayOneShot(_crashSound);
+    _crashParticle.Play();
+    _visualParts.SetActive(false);
     _movement.enabled = false;
+    _movement.StopParticles();
     Invoke(nameof(ReloadLevel), _endDelay);
   }
 
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,9 @@ public class Movement : MonoBehaviour
   [SerializeField] private float _rotationForce;
   
   [SerializeField] private AudioClip _engineSound;
+  [SerializeField] private ParticleSystem _mainThrusterParticle;
+  [SerializeField] private ParticleSystem _leftThrusterParticle;
+  [SerializeField] private ParticleSystem _rightThrusterParticle;
   
   private Rigidbody _rigidbody;
   private AudioSource _audioSource;
@@ -22,20 +25,22 @@ public class Movement : MonoBehaviour
     ProcessRotation();
   }
 
+  public void StopParticles()
+  {
+    _mainThrusterParticle.Stop();
+    _rightThrusterParticle.Stop();
+    _leftThrusterParticle.Stop();
+  }
+
   private void ProcessThrust()
   {
     if (Input.GetKey(KeyCode.Space))
     {
-      _rigidbody.AddRelativeForce(Vector3.up * (_thrustForce * Time.deltaTime));
-      
-      if (_audioSource.isPlaying != true)
-      {
-        _audioSource.PlayOneShot(_engineSound);
-      }
+      StartThrusting();
     }
-    else
+    else if (Input.GetKey(KeyCode.A) == false && Input.GetKey(KeyCode.D) == false)
     {
-      _audioSource.Stop();
+      StopThrusting();
     }
   }
   
@@ -43,18 +48,88 @@ public class Movement : MonoBehaviour
   {
     if (Input.GetKey(KeyCode.A))
     {
-      ApplyRotation(_rotationForce);
+      RotateLeft();
     }
     else if (Input.GetKey(KeyCode.D))
     {
-      ApplyRotation(-_rotationForce);
+      RotateRight();
+    }
+    else
+    {
+      StopRotating();
+    }
+
+    if (Input.GetKey(KeyCode.Space) == false)
+    {
+      _mainThrusterParticle.Stop();
     }
   }
+
+  private void StartThrusting()
+  {
+    _rigidbody.AddRelativeForce(Vector3.up * (_thrustForce * Time.deltaTime));
+      
+    if (_audioSource.isPlaying == false)
+    {
+      _audioSource.PlayOneShot(_engineSound);
+    }
+
+    if (_mainThrusterParticle.isPlaying == false)
+    {
+      _mainThrusterParticle.Play();
+    }
+  }
+
+  private void StopThrusting()
+  {
+    _audioSource.Stop();
+    _mainThrusterParticle.Stop();
+  }
+  
 
   private void ApplyRotation(float rotationThisFrame)
   {
     _rigidbody.freezeRotation = true;
     transform.Rotate(Vector3.forward * (rotationThisFrame * Time.deltaTime));
     _rigidbody.freezeRotation = false;
+  }
+
+  private void RotateLeft()
+  {
+    ApplyRotation(_rotationForce);
+    if (_audioSource.isPlaying == false)
+    {
+      _audioSource.PlayOneShot(_engineSound);
+    }
+    
+    if (_rightThrusterParticle.isPlaying == false)
+    {
+      _rightThrusterParticle.Play();
+    }
+  }
+
+  private void RotateRight()
+  {
+    ApplyRotation(-_rotationForce);
+    if (_audioSource.isPlaying == false)
+    {
+      _audioSource.PlayOneShot(_engineSound);
+    }
+    
+    if (_leftThrusterParticle.isPlaying == false)
+    {
+      _leftThrusterParticle.Play();
+    }
+  }
+
+  private void StopRotating()
+  {
+    _rightThrusterParticle.Stop();
+    _leftThrusterParticle.Stop();
+    
+    if(Input.GetKey(KeyCode.Space) == false)
+    {
+      _audioSource.Stop();
+    }
   }
 }
